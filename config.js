8de0ddd78aa8f740e10909e3f9437ca22c9ba408
Kuvakin Sergey <kuvakin@gmail.com>
2014-10-15 22:28:46 +0400

    init commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    urlEndPoint: 'http://domain/',
+    username: 'username',
+    password: 'password',
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'
+};
