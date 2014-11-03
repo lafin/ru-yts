d9453fd6a21692385a6abba7bc053753fb11d8c1
Kuvakin Sergey <kuvakin@gmail.com>
2014-11-03 23:20:37 +0300

    clean code and fix

--- scraper.js
+++ scraper.js
@@ -1,10 +1,10 @@
 var Client = require('bittorrent-tracker'),
     parseTorrent = require('parse-torrent'),
+    // config = require('./secret'),
     config = require('./config'),
     mongoose = require('mongoose'),
     itemModel = require('./models/Item'),
     Logme = require('logme').Logme,
-    async = require('async'),
     fs = require('fs');
 
 var logFile = fs.createWriteStream(__dirname + '/log.txt', {
@@ -20,43 +20,51 @@ mongoose.connect(config.db);
 var peerId = new Buffer('01234567890123456789');
 var port = 6881;
 
+var checked = 0;
+
+var watcher = function (torrent, item, length) {
+    var client = new Client(peerId, port, torrent);
+    client.start();
+
+    client.once('error', function (err) {
+        logger.error(err);
+        client.stop();
+        if ((checked++) && checked === length) {
+            mongoose.disconnect();
+        }
+    });
+
+    client.once('warning', function (err) {
+        logger.error(err);
+        client.stop();
+        if ((checked++) && checked === length) {
+            mongoose.disconnect();
+        }
+    });
+
+    client.once('update', function (data) {
+        var seeders = data.complete,
+            leechers = data.incomplete;
+        item.info.seeders = seeders;
+        item.info.leechers = leechers;
+        item.save();
+        client.stop();
+        if ((checked++) && checked === length) {
+            mongoose.disconnect();
+        }
+    });
+};
+
 itemModel.find({}, function (error, items) {
     if (error) {
         return logger.error(error);
     }
-
-    var checked = 0;
-
-    items.forEach(function (item) {
-        console.log(item);
+    var length = items.length;
+    for (var i = 0; i < length; i++) {
+        var item = items[i];
         var torrent = parseTorrent(item.info.magnet);
         if (torrent) {
-            var client = new Client(peerId, port, torrent);
-
-            client.start();
-            client.once('error', function (err) {
-                logger.error(err);
-                if ((checked++) && checked == items.length) mongoose.disconnect();
-                client.stop();
-            });
-
-            client.once('warning', function (err) {
-                logger.error(err);
-                if ((checked++) && checked == items.length) mongoose.disconnect();
-                client.stop();
-            });
-
-            client.once('update', function (data) {
-                var seeders = data.complete,
-                    leechers = data.incomplete;
-                console.log('seeders', seeders);
-                console.log('leechers', leechers);
-                item.info.seeders = seeders;
-                item.info.leechers = leechers;
-                item.save();
-                if ((checked++) && checked == items.length) mongoose.disconnect();
-                client.stop();
-            });
+            watcher(torrent, item, length);
         }
-    });
+    }
 });
