f0ae2d742051ea0896d57dd40933c5bfe3beee88
Kuvakin Sergey <kuvakin@gmail.com>
2016-11-14 00:17:28 +0300

    more envs for flexebility

--- server/app.js
+++ server/app.js
@@ -59,7 +59,7 @@ if (onlyWorker) {
         }
     }
 
-    app.listen(3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log('Listening on port %d', this.address().port);
     });
 }
--- server/credential.js
+++ server/credential.js
@@ -1,7 +1,13 @@
-var mongoAddr = process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost';
-var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT || 27017;
+var mongoAddr = process.env.MONGO_PORT_27017_TCP_ADDR || process.env.MONGO_ADDR || 'localhost';
+var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT || process.env.MONGO_PORT || 27017;
+var mongoDbName = process.env.MONGO_DB_NAME || 'db';
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+if (mongoUser && mongoPassword) {
+    mongoAddr = mongoUser + ':' + mongoPassword + '@' + mongoAddr;
+}
 
 module.exports = {
-    db: 'mongodb://' + mongoAddr + ':' + mongoPort + '/db',
+    db: 'mongodb://' + mongoAddr + ':' + mongoPort + '/' + mongoDbName,
     urlEndPoint: 'http://www.torrentino.me/'
 };
